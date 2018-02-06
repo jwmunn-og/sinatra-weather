dd4fd9f29d2cf36fca9fc3356a1634d74f193268
jwmunn <contact@justinmunn.co>
2018-02-05 17:17:04 -0800

    Integrate OpenWeather API, calls with HTTParty, display temp.

--- Gemfile
+++ Gemfile
@@ -1,2 +1,5 @@
 source 'https://rubygems.org'
 gem 'sinatra'
+
+gem 'config_env'
+gem 'httparty'
--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require 'sinatra'
+require 'config_env'
+require 'httparty'
+ConfigEnv.init("#{__dir__}/config/env.rb")
 
 set :port, 3000
+
 get '/' do
+  resp = HTTParty.get(
+    "http://api.openweathermap.org/data/2.5/weather?q=London&APPID=#{ENV['API_KEY']}"
+  )
+  data = JSON.parse(resp.body)
+  @temp = data['main']['temp'] * 9 / 5 - 459.67
   erb :index
 end
