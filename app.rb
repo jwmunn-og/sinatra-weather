2c47dfea79d9d7f553d328b7541d76639223d45c
jwmunn <contact@justinmunn.co>
2018-02-05 18:01:28 -0800

    Added different cities and weather icons.

--- app.rb
+++ app.rb
@@ -2,12 +2,24 @@ require 'sinatra'
 require 'config_env'
 require 'httparty'
 require_relative 'models/weather.rb'
+require 'uri'
+
 ConfigEnv.init("#{__dir__}/config/env.rb")
 
 set :port, 3000
 
 get '/' do
-  weather = Weather.new
+  @cities = [
+    "Seattle",
+    "San Francisco",
+    "London",
+    "Tokyo",
+    "Cairo"
+  ]
+
+  @city = params[:city] || "San Francisco"
+  weather = Weather.new(@city)
   @temp = weather.temp
+  @icons = weather.icons
   erb :index
 end
--- models/weather.rb
+++ models/weather.rb
@@ -1,9 +1,14 @@
 require 'httparty'
+require 'uri'
 
 class Weather
+  def initialize(city)
+    @city = city
+  end
+
   def weather_data
     resp = HTTParty.get(
-      "http://api.openweathermap.org/data/2.5/weather?q=London&APPID=#{ENV['API_KEY']}"
+      "http://api.openweathermap.org/data/2.5/weather?q=#{URI.escape(@city)}&APPID=#{ENV['API_KEY']}"
     )
 
     JSON.parse(resp.body)
@@ -14,6 +19,14 @@ class Weather
     return nil if data.nil? || data['main'].nil? || data['main']['temp'].nil?
 
     data['main']['temp'] * 9 / 5 - 459.67
+  end
+
+  def icons
+    data = weather_data
+    return [] if data.nil? || data['weather'].nil?
+    data['weather'].collect do |weather|
+      weather['icon'] + ".png"
+    end.uniq
   end
 
 end
